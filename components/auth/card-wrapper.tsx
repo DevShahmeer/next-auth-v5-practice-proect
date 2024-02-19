25bb567fe0ced3d15d7834eb2fa6c65597906c72
shahmeer <theshahmeerfsd@gmail.com>
2024-02-19 21:49:16 +0500

    Login Page Added

---
+++ components/auth/card-wrapper.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardFooter,
+} from "@/components/ui/card";
+
+interface CardWrapperProps {
+  children: React.ReactNode;
+  headerLabel: string;
+  backButtonLabel: string;
+  backButtonHref: string;
+  showSocial?: boolean;
+}
+
+export const CardWrapper = ({
+  children,
+  headerLabel,
+  backButtonLabel,
+  backButtonHref,
+  showSocial,
+}: CardWrapperProps) => {
+  return <Card className="w-[400px] shadow-md">{children}</Card>;
+};
