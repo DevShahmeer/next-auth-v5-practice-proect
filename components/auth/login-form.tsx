649285c063ab5eae03cbf9bb3aa4af918f90a5d2
shahmeer <theshahmeerfsd@gmail.com>
2024-02-21 23:33:55 +0500

    login page and ui add

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,10 +1,23 @@
-import { CardWrapper } from "./card-wrapper";
+import { CardWrapper } from "@/components/auth/card-wrapper";
+
+import { zodResolver } from "@hookform/resolvers/zod"; }
+import {useForm} from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/components/ui/form";
 
 export const LoginForm = () => {
+
+  const form = useForm
   return (
     <CardWrapper
       headerLabel="Welcome back"
-      backButtonLabel="Don't have an account"
+      backButtonLabel="Don't have an account?"
       backButtonHref="/auth/register"
       showSocial
     >
